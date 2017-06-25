122b280f3df3a71294a5d7e813aafacc0d862667
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-06-25 07:03:01 +0530

    created user schema

---
+++ src/models/UserSchema.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+let StockSchema = new Schema({
+  StartDate: {
+    type: Date,
+    min: Date("1970-01-01"),
+    required: true
+  },
+  EndDate: {
+    type: Date,
+    min: Date("1970-01-01"),
+    required: true
+  },
+  LeaveType: {
+    type: String,
+    enum: ["Formal", "Informal"],
+    required: true
+  },
+  Reason: {
+    type: String,
+    required: true
+  },
+  RequestBy: {
+    type: String,
+    required: true
+  },
+  RequestedAt: {
+    type: Date,
+    min: Date("1970-01-01"),
+    required: true
+  },
+  ApprovalStatus: {
+    type: String,
+    enum: ["Approved", "Not Approved"],
+    required: true
+  },
+  ApprovedAt: {
+    type: Date,
+    min: Date("1970-01-01")
+  }
+});
+
+module.exports = mongoose.model("User", StockSchema);
